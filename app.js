52a31326e25c07c44c3023914d19c95b829332e4
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2025-01-10 17:52:26 +0200

    add claude improvements to the CSS

--- app.js
+++ app.js
@@ -1,48 +1,71 @@
 // Constants
 
+const display = document.querySelector("#display");
+const buttonsContainer = document.querySelector("#buttons-container");
+const buttons = document.querySelectorAll("input[type=button]");
+
 // Variables
-let number1, number2;
-let operator = ["+", "-", "*", "/", "="];
+let operand = 0,
+  result = 0;
+let operator = ["+", "-", "x", "/", "="];
 
 // Functions
-
-function add(number1, number2) {
-    return number1 + number2;
+function add(operand1, operand2) {
+  return operand1 + operand2;
 }
 
-function subtract(number1, number2) {
-    return number1 - number2;
+function subtract(operand1, operand2) {
+  return operand1 - operand2;
 }
 
-function multiply(number1, number2) {
-    return number1 * number2;
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
 }
 
-function divide(number1, number2) {
-    return number1 / number2;
+function divide(operand1, operand2) {
+  return operand1 / operand2;
 }
 
 function operate(operator, operand1, operand2) {
-    switch (operator) {
-        case "+":
-            return add(operand1, operand2);
-            break;
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+      break;
 
-        case "-":
-            return subtract(operand1, operand2);
-            break;
+    case "-":
+      return subtract(operand1, operand2);
+      break;
 
-        case "*":
-            return multiply(operand1, operand2);
-            break;
+    case "x":
+      return multiply(operand1, operand2);
+      break;
 
-        case "/":
-            return divide(operand1, operand2);
-            break;
+    case "/":
+      return divide(operand1, operand2);
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
+
+function deleteAll() {
+  display.value = "0";
+}
+
+function addGlobalEventListener(type, selector, callback, parent = document) {
+  parent.addEventListener(type, (e) => {
+    if (e.target.matches(selector)) {
+      callback(e);
+    }
+  });
+}
+
 // Create the functions that populate the display when you click the digit buttons. You should store the content of the display (the number) in a variable for use in the next step.
 
+function populateDisplay(input) {
+  display.value += input;
+  operand += display.value;
+}
+
+addGlobalEventListener("click");
